43d2ea75e71048219636a00bb79715cbb5b3fc6e
Gabriel <gabrielmarotacampos221@gmail.com>
2024-08-21 09:04:29 -0300

    Alteração no Index
    Alteração que agora ao invés do index conter todo conteúdo do "Header" e "Footer", eles serão incluídos com o uso do PHP

---
+++ Projeto/VIEW/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+		integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="../CSS/style.css">
+	<link rel="shortcut icon" href="../IMG/Utilitários/Logo.png" type="image/x-icon">
+
+	<title>Senac Chaves</title>
+
+	<!-- MODIFICAÇÃO TEMPORÁRIA PRO FOOTER FICAR TRAVADO EM BAIXO DA TELA -->
+	<!-- MODIFICAÇÃO TEMPORÁRIA PRO FOOTER FICAR TRAVADO EM BAIXO DA TELA -->
+	<style>
+		footer{
+			position: fixed;
+		}
+	</style>
+	<!-- MODIFICAÇÃO TEMPORÁRIA PRO FOOTER FICAR TRAVADO EM BAIXO DA TELA -->
+	<!-- MODIFICAÇÃO TEMPORÁRIA PRO FOOTER FICAR TRAVADO EM BAIXO DA TELA -->
+
+</head>
+
+<body>
+	<?php
+		include_once "Header.php"
+	?>
+	<?php
+		include_once "Footer.php"
+	?>
+</body>
+</html>
