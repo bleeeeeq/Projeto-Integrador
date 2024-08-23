2320b9affa05128dd6581d9ef163c3e1a2691821
Gabriel <gabrielmarotacampos221@gmail.com>
2024-08-23 12:06:40 -0300

    Atualização Index
    Incluindo o header e footer por meio do php
    Adição scripts do Bootstrap

--- Projeto/VIEW/index.php
+++ Projeto/VIEW/index.php
@@ -6,30 +6,38 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-		integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link rel="stylesheet" href="../CSS/style.css">
-	<link rel="shortcut icon" href="../IMG/Utilitários/Logo.png" type="image/x-icon">
-
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/style.css">
+	<link rel="shortcut icon" href="../IMG/Utilitários/Logo.png" type="image/x-icon">	
 	<title>Senac Chaves</title>
-
-	<!-- MODIFICAÇÃO TEMPORÁRIA PRO FOOTER FICAR TRAVADO EM BAIXO DA TELA -->
-	<!-- MODIFICAÇÃO TEMPORÁRIA PRO FOOTER FICAR TRAVADO EM BAIXO DA TELA -->
-	<style>
-		footer{
-			position: fixed;
-		}
-	</style>
-	<!-- MODIFICAÇÃO TEMPORÁRIA PRO FOOTER FICAR TRAVADO EM BAIXO DA TELA -->
-	<!-- MODIFICAÇÃO TEMPORÁRIA PRO FOOTER FICAR TRAVADO EM BAIXO DA TELA -->
-
 </head>
 
+
+<style>
+    footer{
+        position: fixed;
+    }
+</style>
+
 <body>
-	<?php
+    <?php
 		include_once "Header.php"
 	?>
-	<?php
-		include_once "Footer.php"
-	?>
+
+    <?php
+        include_once "Footer.php";
+    ?>
+
+    <!-- script -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+   <!-- script -->
 </body>
 </html>
