fc794c223226394ab4e8dae29e370f8fcc91a320
Gabriel <gabrielmarotacampos221@gmail.com>
2024-08-23 12:07:50 -0300

    Atualização tabela chaves
    Remoção do CSS interno

--- Projeto/VIEW/tabChaves.php
+++ Projeto/VIEW/tabChaves.php
@@ -14,74 +14,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/tabela.css">
     <title>Senac Chaves</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-        .container {
-            width: 80%;
-            margin-top: 50px;
-        }
-        h1 {
-            text-align: center;
-            margin-bottom: 20px;
-        }
-        .search-form {
-            display: flex;
-            justify-content: space-between;
-            margin-bottom: 20px;
-        }
-        .search-form input[type="text"] {
-            width: 48%;
-            padding: 10px;
-            font-size: 16px;
-        }
-        .search-form button {
-            background-color: #ffb509;
-            padding: 10px 20px;
-            font-size: 16px;
-        }
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-bottom: 50px;
-        }
-        th, td {
-            background-color: white;
-            padding: 12px;
-            border: 1px solid #ccc;
-            text-align: left;
-            background-color: rgba(104, 163, 252, 0.596);
-        }
-        th {
-            background-color: rgba(18, 99, 219, 0.596);
-        }
-        .all-classes-button {
-            display: flex;
-            justify-content: flex-end;
-        }
-        .all-classes-button button {
-            padding: 10px 20px;
-            font-size: 16px;
-            background-color: #ffb509;
-        }
-    </style>
 </head>
 
 <body>
     <?php
-        //include_once "Header.php";
+        include_once "Header.php";
     ?>
 
+
+<div class="bTable">
+    
     <div class="container">
-        <h1>Chaves do gigantesco senacs chaves nozama</h1>
+        <h1>chave</h1>
         <form class="search-form" method="POST" action="../PHP/tabelaChaves.php">
             <input type="text" name="andar" placeholder="Andar">
             <input type="text" name="numero" placeholder="Número da Sala">
@@ -110,7 +58,7 @@
                 <?php
                     if(isset($_SESSION['results'])){
                         echo $_SESSION['results'];
-                        unset($_SESSION['results']);
+                     //   unset($_SESSION['results']);
                     }
                 ?>
             </tbody>
@@ -121,10 +69,7 @@
                 <button type="submit" name="all_classes">Todas as Chaves</button>
             </form>
         </div>
-
     </div>
-    <?php
-        include_once "Footer.php";
-    ?>
+</div>
 </body>
 </html>
