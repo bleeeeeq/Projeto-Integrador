c102f8cc568c282f3ca87f13472b113560d8d822
Gabriel <gabrielmarotacampos221@gmail.com>
2024-09-02 11:54:44 -0300

    Otimização de código
    Removendo linhas desnecessárias

--- Projeto/PHP/agendarChave.php
+++ Projeto/PHP/agendarChave.php
@@ -4,30 +4,30 @@ include 'database.php';
 
 if(isset($_POST['Agendar'])){
             
-            $numSala = filter_input(INPUT_POST, 'numSala', FILTER_SANITIZE_STRING);
-            $nome = filter_input(INPUT_POST, 'nomeSala', FILTER_SANITIZE_STRING);
-            $data = filter_input(INPUT_POST, 'data', FILTER_SANITIZE_STRING);
-            $hora = filter_input(INPUT_POST, 'hora', FILTER_SANITIZE_STRING);
-            $nomeUsuario = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
-            $emuso = filter_input(INPUT_POST, 'emuso', FILTER_SANITIZE_STRING);
-            $idsala = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
-        
-    
-            $declaracaoBD = $conexao -> prepare("INSERT INTO `historicochave`(`chave`,`nome`,`data`,`hora`,`nomeUsuario`)VALUES(?,?,?,?,?)");
-            $declaracaoBD -> bind_param("sssss", $numSala, $nome, $data, $hora, $nomeUsuario);
-            $declaracaoBD -> execute();
+    $numSala = filter_input(INPUT_POST, 'numSala', FILTER_SANITIZE_STRING);
+    $nome = filter_input(INPUT_POST, 'nomeSala', FILTER_SANITIZE_STRING);
+    $data = filter_input(INPUT_POST, 'data', FILTER_SANITIZE_STRING);
+    $hora = filter_input(INPUT_POST, 'hora', FILTER_SANITIZE_STRING);
+    $nomeUsuario = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+    $emuso = filter_input(INPUT_POST, 'emuso', FILTER_SANITIZE_STRING);
+    $idsala = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
 
-            $declaracaoBD = $conexao -> prepare("UPDATE `agenda` SET  `nomeUsuario` = ? , `emuso` = ? WHERE `idagenda` = ?");
-            $declaracaoBD -> bind_param("ssi", $nomeUsuario, $emuso, $idsala);
+    
+    $declaracaoBD = $conexao -> prepare("INSERT INTO `historicochave`(`chave`,`nome`,`data`,`hora`,`nomeUsuario`)VALUES(?,?,?,?,?)");
+    $declaracaoBD -> bind_param("sssss", $numSala, $nome, $data, $hora, $nomeUsuario);
+    $declaracaoBD -> execute();
+
+    $declaracaoBD = $conexao -> prepare("UPDATE `agenda` SET  `nomeUsuario` = ? , `emuso` = ? WHERE `idagenda` = ?");
+    $declaracaoBD -> bind_param("ssi", $nomeUsuario, $emuso, $idsala);
             
-            if ($declaracaoBD -> execute()){
-                echo "<script>
-                        alert('Perfeito! Agora você pode pegar sua chave!');
-                        window.location.href = '../PHP/cadUsuario.php';
-                    </script>;";
-            }
-            else{
-                echo "Erro ao realizar o agendamento da chave: ". $declaracaoBD->error;
-            }
-            $declaracao->close();
+    if ($declaracaoBD -> execute()){
+         echo "<script>
+                 alert('Perfeito! Agora você pode pegar sua chave!');
+                 window.location.href = '../PHP/cadUsuario.php';
+             </script>;";
+     }
+     else{
+        echo "Erro ao realizar o agendamento da chave: ". $declaracaoBD->error;
+    }
+     $declaracaoBD->close();
 }
