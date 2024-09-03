6fb77c86419e631741b0aece5ef020bb9ddb8c74
Gabriel <gabrielmarotacampos221@gmail.com>
2024-09-03 09:49:00 -0300

    Adicionando o Header via include

--- Projeto/VIEW/login.php
+++ Projeto/VIEW/login.php
@@ -15,52 +15,12 @@
 </head>
  
 <body>
-    <!-- Icon -->
-    <nav class="navbar navbar-light" style="background-color: #ffffff;">
-        <a class="navbar-brand" href="#">
-            <img src="../IMG/Utilitários/LogoCaC.png" width=150 height="30" class="d-inline-block align-top" alt="">
-        </a>
-    </nav>
-    <!-- Icon -->
+
  
     <!-- Home -->
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #ffb509;">
-        <a class="navbar-brand" href="#"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
-            <span class="navbar-toggler-icon"></span>
-        </button>
- 
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
- 
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.html">Home<span class="sr-only">(Página atual)</span></a>
-                </li>
- 
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Atualizações</a>
-                </li>
- 
-                <li class="nav-item">
-                    <a class="nav-link" href="cadUsuario.php">Cadastro</a>
-                </li>
- 
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Chaves
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Verificar chaves</a>
-                        <a class="dropdown-item" href="#">Realizar agendamento</a>
-                        <a class="dropdown-item" href="#">Perdi minha chave</a>
-                    </div>
-                </li>
- 
-            </ul>
-        </div>
-    </nav>
+    <?php
+        include_once "Header.php";
+    ?>
     <!-- Home -->
  
     <!--Tela Login-->
