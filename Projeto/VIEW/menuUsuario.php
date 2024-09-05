9ad297cc0acbd181fb4897e20b3157ed2acf99ae
Gabriel <gabrielmarotacampos221@gmail.com>
2024-09-05 11:45:21 -0300

    Atualização menus
    Agora os menus foram separados, para o usuário ter acessos a menus específicos e gerentes outras opções

---
+++ Projeto/VIEW/menuUsuario.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Senac Chaves</title>
+</head>
+<body>
+<div id="ItensMenu">
+        <a href="registroChaves.php">Pegar chave</a>
+        <a href="devolverChave.php">Devolver chave</a>
+        <a href="esqueciChave.php">Perdi minha chave</a>
+</div>
+</body>
+</html>
